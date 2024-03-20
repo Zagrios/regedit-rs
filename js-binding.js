f7b5b238426834ddbc12d7a919d9357170c9667e
MathieuG-P <40181755+Zagrios@users.noreply.github.com>
2024-03-20 13:54:32 +0100

    [chore] update deps

--- js-binding.js
+++ js-binding.js
@@ -266,6 +266,20 @@ switch (platform) {
           }
         }
         break
+      case 's390x':
+        localFileExisted = existsSync(
+          join(__dirname, 'regedit-rs.linux-s390x-gnu.node')
+        )
+        try {
+          if (localFileExisted) {
+            nativeBinding = require('./regedit-rs.linux-s390x-gnu.node')
+          } else {
+            nativeBinding = require('regedit-rs-linux-s390x-gnu')
+          }
+        } catch (e) {
+          loadError = e
+        }
+        break
       default:
         throw new Error(`Unsupported architecture on Linux: ${arch}`)
     }
